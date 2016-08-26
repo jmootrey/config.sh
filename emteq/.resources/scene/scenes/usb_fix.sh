de1c935588046c937b39d0e7a4f4b816808fc37d
jmootrey <jmootrey@gmail.com>
2016-08-26 12:42:58 -0500

    binary update

--- emteq/.resources/scene/scenes/usb_fix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# SCRIPTID 22
-# Correct write permissions for USB mount
-# TYPE 0
-# STARTUP 1
-# EXCLUSIVE 0
-cp /opt/scenes/usb_mount.sh /opt/eConnect/scripts/usb/
-cp /opt/scenes/tilestreamstart.sh /root
